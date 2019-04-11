cd47171d64dc2b134f6c983a2800820c28677b53
Speeder <ek.speeder@gmail.com>
2019-04-11 19:24:44 +0200

    Added the NNG-version of the LLVM v8.0 setup

--- AutoSetup.bat
+++ AutoSetup.bat
@@ -20,23 +20,17 @@ if not defined VS_DIR (echo Cannot find Visual Studio 2017 directory! & exit /b 
 
 set VS_PROOT_DIR=%VS_DIR%\Common7\IDE\VC\VCTargets\Platforms
 
-if not exist "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017" (mkdir "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017")
-if not exist "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017_xp" (mkdir "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017_xp")
-copy Toolset-llvm-vs2017-x86.props "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017\Toolset.props"
-copy Toolset-llvm-vs2017-x86.targets "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017\Toolset.targets"
-copy Toolset-llvm-vs2017-xp-x86.props "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017_xp\Toolset.props"
-copy Toolset-llvm-vs2017-xp-x86.targets "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM-vs2017_xp\Toolset.targets"
+if not exist "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM8-vs2017" (mkdir "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM8-vs2017")
+copy Toolset-llvm-vs2017-x86.props "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM8-vs2017\Toolset.props"
+copy Toolset-llvm-vs2017-x86.targets "%VS_PROOT_DIR%\Win32\PlatformToolsets\LLVM8-vs2017\Toolset.targets"
 
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto install_x64
 if not "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto done
 
 :install_x64
-if not exist "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017" (mkdir "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017")
-if not exist "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017_xp" (mkdir "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017_xp")
-copy Toolset-llvm-vs2017-x64.props "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017\Toolset.props"
-copy Toolset-llvm-vs2017-x64.targets "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017\Toolset.targets"
-copy Toolset-llvm-vs2017-xp-x64.props "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017_xp\Toolset.props"
-copy Toolset-llvm-vs2017-xp-x64.targets "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM-vs2017_xp\Toolset.targets"
+if not exist "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM8-vs2017" (mkdir "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM8-vs2017")
+copy Toolset-llvm-vs2017-x64.props "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM8-vs2017\Toolset.props"
+copy Toolset-llvm-vs2017-x64.targets "%VS_PROOT_DIR%\x64\PlatformToolsets\LLVM8-vs2017\Toolset.targets"
 goto done
 
 :done
